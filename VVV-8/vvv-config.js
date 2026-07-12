422e54ad2247aed17b3325d8a10c0f814e7615b7
Manska0770 <prostopro-ok1@rambler.ru>
2026-07-12 08:24:27 +0000

    Remove project files and keep only render settings

--- VVV-8/vvv-config.js
+++
@@ -1,5 +0,0 @@
-window.VVV_SUPABASE_CONFIG = {
-  SUPABASE_URL: 'https://fcmnytratjicextoywyc.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_D2GzJ59tznrquQCnmFzoxQ_goeBiU8g',
-  TELEGRAM_BOT_USERNAME: '' // Укажите имя бота без @, например 'MyBot'
-};
